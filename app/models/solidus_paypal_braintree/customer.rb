ab9a417ee618c3d2ea3b270e2d86aeecb2e6226a
Ryan Woods <contact@ryanofwoods.com>
2021-09-27 16:10:50 +0200

    Make the gem compatible with Rails 4.2
    The option `optional` was added in Rails 5.0.

--- app/models/solidus_paypal_braintree/customer.rb
+++ app/models/solidus_paypal_braintree/customer.rb
@@ -2,7 +2,7 @@
 
 module SolidusPaypalBraintree
   class Customer < ApplicationRecord
-    belongs_to :user, class_name: ::Spree::UserClassHandle.new, optional: true
+    belongs_to :user, class_name: ::Spree::UserClassHandle.new
     has_many :sources, class_name: "SolidusPaypalBraintree::Source", inverse_of: :customer, dependent: :destroy
   end
 end
